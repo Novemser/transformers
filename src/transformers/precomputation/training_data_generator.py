eca1b4f68554c151e4c5943931d3b8d3612f231c
Novemser <novemser@gmail.com>
2024-05-11 17:33:16 +0800

    Improve efficiency of a_hat computation

--- src/transformers/precomputation/training_data_generator.py
+++ src/transformers/precomputation/training_data_generator.py
@@ -5,8 +5,8 @@ from tqdm import tqdm
 import torch.nn as nn
 
 def P_i_j_k(i_index: int, j_index: int, k_index: int, up_proj: torch.Tensor, down_proj: torch.Tensor):
-    # return up_proj[j_index][k_index] * down_proj[k_index][i_index]
-    return up_proj[k_index][j_index] * down_proj[i_index][k_index]
+    return up_proj[j_index][k_index] * down_proj[k_index][i_index]
+    # return up_proj[k_index][j_index] * down_proj[i_index][k_index]
 
 
 def generate_act_hats_llama2(
@@ -20,18 +20,17 @@ def generate_act_hats_llama2(
     print(f"num_labels: {num_labels}")
     output = None
     for lable_id in tqdm(range(num_labels), desc="Processing samples:"):
-        activation = record_act_result[lable_id]
+        activation = record_act_result[lable_id].to(device=up_proj.device)
         assert activation.shape[0] == d_intermediate, "Unexpected recorded activation shape!"
         # calculate a_hat_i_j
         for i_index in tqdm(range(d_model), desc="Processing i_index"):
             for j_index in tqdm(range(d_model), desc="Processing j_index"):
-                upper = 0
-                lower = 0
-                for k_index in tqdm(range(d_intermediate), desc="Processing k_index"):
-                    p_i_j_k = P_i_j_k(i_index=i_index, j_index=j_index, k_index=k_index, up_proj=up_proj, down_proj=down_proj)
-                    upper += activation[k_index] * p_i_j_k
-                    lower += p_i_j_k
-                a_hat_i_j = (upper / lower).unsqueeze(-1)
+                u_jth_row = up_proj[j_index, :]#.unsqueeze(0)
+                o_ith_col = down_proj[:, i_index]#.unsqueeze(1)
+                mul = u_jth_row * o_ith_col
+                c_ij = mul.sum()
+                numerator = (activation * mul).sum()
+                a_hat_i_j = (numerator / c_ij).unsqueeze(-1)
                 if output == None:
                     output = a_hat_i_j
                 else:
